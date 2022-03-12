af9eb672c992213b7d95f17236599367c013963d
Ynikita <96648626+Ynikita@users.noreply.github.com>
2022-03-12 16:00:00 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
-
+const PORT = process.env.PORT || 80
 dotenv.config();
 
 let initialPath = path.join(__dirname, 'pablik')
@@ -15,6 +15,37 @@ app.get('/',(req,res)  => {
     res.sendFile(path.join(initialPath, 'index.html'));
 })
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log("i'm ready to kill @_#");
+})
+
+app.post("/mail", (req, res)=>{
+    const {
+        firstname, lastname, email, msg
+    } = req.body;
+    const transporter = nodemailer.createTransport({
+        service: 'mail.ru',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    }) 
+
+    const mailOptions = {
+        from: 'yakunovoo@mail.ru',
+        to: 'yakunovoo@mail.ru',
+        subject: 'ВАМ ПОВЕСТКА',
+        text: `First name: ${firstname}, \nLast name: ${lastname}, \nEmail: ${email}, \nMessage: ${msg}`
+        }
+
+
+    transporter.sendMail(mailOptions,(err,result)=>{
+        if(err){
+            console.log(err);
+            res.json("ОШИБКА СТОП 00000000000000000000!")
+        }
+        else{
+            req.json("ВСЁ ОТЛИЩЩЩНА!")
+        }
+    })
 })
